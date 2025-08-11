1ee78c8a1f3d399be84f48e3fc8c87e46e4c2a7a
danplanned <154644252+danplanned@users.noreply.github.com>
2025-08-11 10:42:38 -0700

    Add files via upload
    first commit with hello files

---
+++ helloProject/target/debug/deps/helloProject.d
@@ -0,0 +1,5 @@
+D:\Software\BlockChain\Rust\helloProject\target\debug\deps\helloProject.d: src\main.rs
+
+D:\Software\BlockChain\Rust\helloProject\target\debug\deps\helloProject.exe: src\main.rs
+
+src\main.rs:
---
+++ helloProject/target/debug/helloProject.d
@@ -0,0 +1,1 @@
+D:\Software\BlockChain\Rust\helloProject\target\debug\helloProject.exe: D:\Software\BlockChain\Rust\helloProject\src\main.rs
